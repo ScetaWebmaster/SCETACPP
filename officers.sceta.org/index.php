151f9b4d74b32caf0011cf291ac9b4b355894468
ByronPhung <ByronPhung@users.noreply.github.com>
2015-02-21 14:40:14 -0800

    Fixed index.php not redirecting to login.php when not signed in by putting "Header" tag before "HTML" tag.

--- officers.sceta.org/index.php
+++ officers.sceta.org/index.php
@@ -2,6 +2,10 @@
     include_once 'inc/db_connect.php';
     include_once 'inc/functions.php';
     sec_session_start();
+
+    if (login_check($mysqli) == FALSE) {
+        header('Location: login.php');
+    }
 ?>
 
 <!DOCTYPE html>
@@ -13,25 +17,26 @@
     </head>
 
     <body>
-        <!-- If User is Signed In -->
-        <?php if (login_check($mysqli) == true) : ?>
-        <div class="bodycontainer">
-            <div class="maincontainer">
-                <h4>SCETA Officers Control Panel</h4>
-                <hr>
-                <p>Welcome <?php echo htmlentities($_SESSION['username']); ?>! (<a href="inc/logout.php">Logout</a>)</p>
-                <hr>
-                <p>
-                    <b>Available Actions</b>
-                    <ul>
-                        <li><a href="update_parts.php">Update Parts</a></li>
-                    </ul>
-                </p>
-            </div>
-        </div>
-
-        <!-- If User is NOT Signed In -->
-        <?php else : header('Location: login.php'); ?>
-        <?php endif; ?>
+        <?php 
+            // If the User is Signed In
+            if (login_check($mysqli) == true) {
+                echo "<div class='bodycontainer'>
+                        <div class='maincontainer'>
+                            <h4>SCETA Officers Control Panel</h4>
+                            <hr>
+                            <p>Welcome ";
+                echo htmlentities($_SESSION['username']);
+                echo '! (<a href="inc/logout.php">Logout</a>)</p>
+                            <hr>
+                            <p>
+                                <b>Available Actions</b>
+                                <ul>
+                                    <li><a href="update_parts.php">Update Parts</a></li>
+                                </ul>
+                            </p>
+                        </div>
+                    </div>';
+            }
+        ?>
     </body>
 </html>
