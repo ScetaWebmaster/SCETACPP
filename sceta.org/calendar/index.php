2f1dbb10f4b11635556aa6e1f7c95040d5f66f0a
ByronPhung <ByronPhung@users.noreply.github.com>
2015-03-18 18:29:09 -0700

    Fixed responsive style of the full calendar.

--- sceta.org/calendar/index.php
+++ sceta.org/calendar/index.php
@@ -71,7 +71,8 @@
 							</p>
 
 							<h4>Full Calendar</h4>
-							<iframe src="https://www.google.com/calendar/embed?showCalendars=0&amp;showPrint=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=scetacpp%40gmail.com&amp;color=%23125A12&amp;ctz=America%2FLos_Angeles" style="border-width: 0; padding: 0 24px 0 24px;" width="95%" height="600" frameborder="0" scrolling="no"><img src="../img/ic_loading.gif"></iframe>
+							<iframe src="https://www.google.com/calendar/embed?showTabs=0&amp;showCalendars=0&amp;showPrint=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=scetacpp%40gmail.com&amp;color=%23125A12&amp;ctz=America%2FLos_Angeles" 
+								style="width: 94%; padding-left: 2.7%;" height="600" frameborder="0" scrolling="no"><img src="../img/ic_loading.gif"></iframe>
 							<br><br>
 							
 							<h4>Full Agenda</h4>
